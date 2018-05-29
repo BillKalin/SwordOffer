b77740fb786939ebbb23ff7f4b48afe63d0243ab
BillKalin <396116131@qq.com>
2018-05-29 21:07:19 +0800

    init question10

--- sourcecode/src/main/java/com/billkalin/sourcecode/question10/Main.java
+++ sourcecode/src/main/java/com/billkalin/sourcecode/question10/Main.java
@@ -3,6 +3,29 @@ package com.billkalin.sourcecode.question10;
 public class Main {
 
     public static void main(String[] args) {
+        printMaxNum(2);
+    }
 
+    private static void printMaxNum(int n) {
+        if (n <= 0) {
+            return;
+        }
+
+        int num = 1;
+        int bit = 1;
+        while (bit <= n) {
+            num *= 10;
+            bit++;
+        }
+
+        for (int i = 0; i < num; i++) {
+            System.out.print(i + ",");
+        }
+    }
+
+    private static void printMaxNum2(int n) {
+        if (n <= 0) {
+            return;
+        }
     }
 }
