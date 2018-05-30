a206bf8eb90c58c5c05eea4005484eab53debacb
BillKalin <396116131@qq.com>
2018-05-30 10:15:24 +0800

    init question10

--- sourcecode/src/main/java/com/billkalin/sourcecode/question10/Main.java
+++ sourcecode/src/main/java/com/billkalin/sourcecode/question10/Main.java
@@ -3,7 +3,9 @@ package com.billkalin.sourcecode.question10;
 public class Main {
 
     public static void main(String[] args) {
-        printMaxNum(2);
+        printMaxNum(2);//暴力遍历
+        printMaxNum2(3);//使用数组保存对应位的数字
+        printMaxNum3(3);//使用递归方式
     }
 
     private static void printMaxNum(int n) {
@@ -21,8 +23,6 @@ public class Main {
         for (int i = 1; i < num; i++) {
             System.out.print(i + ",");
         }
-
-        printMaxNum2(3);
     }
 
     private static void printMaxNum2(int n) {
@@ -38,6 +38,12 @@ public class Main {
         }
     }
 
+    /**
+     * 使用数组保存对应位置的数字，同时考虑了进位的问题
+     * @param n
+     * @param numArr
+     * @return
+     */
     private static boolean increment(int n, int[] numArr) {
         for (int i = n - 1; i >= 0; i--) {
             int num = numArr[i] + 1;
@@ -52,6 +58,34 @@ public class Main {
             }
         }
         return false;
+    }
+
+    private static void printMaxNum3(int n) {
+        if (n <= 0) {
+            return;
+        }
+
+        int[] numArr = new int[n];
+        printMaxNum3(numArr, 0, n);
+        System.out.println("");
+    }
+
+    /**
+     * 使用递归方式,产生全排列
+     * @param arr
+     * @param index
+     * @param len
+     */
+    private static void printMaxNum3(int[] arr, int index, int len) {
+        if (index == len - 1) {
+            printNum(arr);
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            arr[index + 1] = i;
+            printMaxNum3(arr, index + 1, len);
+        }
     }
 
     private static void printNum(int[] numArr) {
