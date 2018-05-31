113d2d2569668b68a7956feeb7effdd0a9e39891
BillKalin <396116131@qq.com>
2018-05-31 23:15:00 +0800

    question13

--- sourcecode/src/main/java/com/billkalin/sourcecode/question13/Main.java
+++ sourcecode/src/main/java/com/billkalin/sourcecode/question13/Main.java
@@ -7,13 +7,24 @@ public class Main {
 
     public static void main(String[] args) {
         SimpleLinkList<Integer> linkList = new SimpleLinkList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 4; i++) {
             linkList.add(i);
         }
+
         LinkNode<Integer> head = linkList.getHead();
         printLinkList(head);
         int k = 10;
         LinkNode<Integer> node = findSpecialNode(head, k);
+
+        //查找中间节点
+//      LinkNode<Integer> node2 = findCenterNode(head);
+
+        //判断是否有环
+//      LinkNode<Integer> last = linkList.getNode(3);
+//      LinkNode<Integer> b = linkList.getNode(1);
+//      last.next = b;
+//      boolean isSuccess = hasCircle(head);
+
         System.out.println(String.format("\n链表第%d个结点是：%d", k, (node == null ? -1 : node.t)));
     }
 
@@ -57,5 +68,49 @@ public class Main {
             System.out.print(curr.t + ", ");
             curr = curr.next;
         }
+    }
+
+    /**
+     * 查找中间节点
+     * @param head
+     * @return
+     */
+    private static LinkNode<Integer> findCenterNode(LinkNode<Integer> head) {
+        if (head == null)
+            return null;
+
+        LinkNode<Integer> p1 = head;
+        LinkNode<Integer> p2 = head.next;
+
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+
+        return p1;
+    }
+
+    /**
+     * 判断单链表是否有环
+     * @param head
+     * @return
+     */
+    private static boolean hasCircle(LinkNode<Integer> head) {
+        if (head == null)
+            return false;
+
+        LinkNode<Integer> p1 = head;
+        LinkNode<Integer> p2 = head.next;
+
+        while (null != p2 && p2.next != null) {
+            if (p1 == p2) {
+                return true;
+            }
+            p1 = p1.next;
+            p2 = p2.next.next;
+
+        }
+
+        return false;
     }
 }
