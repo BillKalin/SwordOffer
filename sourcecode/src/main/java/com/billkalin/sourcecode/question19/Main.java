0d87620206b4529d3eefc3b80c9628e68fcf5de1
BillKalin <396116131@qq.com>
2018-06-04 23:06:31 +0800

    fix question 19

--- sourcecode/src/main/java/com/billkalin/sourcecode/question19/Main.java
+++ sourcecode/src/main/java/com/billkalin/sourcecode/question19/Main.java
@@ -1,8 +1,65 @@
 package com.billkalin.sourcecode.question19;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
+        StackPro stackPro = new StackPro();
+        stackPro.push(3);
+        stackPro.push(4);
+        stackPro.push(2);
+        stackPro.push(5);
+        stackPro.push(1);
 
+        System.out.println("min = " + stackPro.min());
+
+        stackPro.pop();
+
+        System.out.println("min = " + stackPro.min());
+
+        stackPro.pop();
+        stackPro.pop();
+        stackPro.pop();
+        stackPro.pop();
+        stackPro.pop();
+
+    }
+
+    private static class StackPro {
+        private Stack<Integer> srcStack;
+        private Stack<Integer> helpStack;
+
+        public StackPro() {
+            srcStack = new Stack<>();
+            helpStack = new Stack<>();
+        }
+
+        public void push(int value) {
+            if (helpStack.empty() || helpStack.peek() > value) {
+                helpStack.push(value);
+            } else {
+                helpStack.push(helpStack.peek());
+            }
+            srcStack.push(value);
+        }
+
+        public int pop() {
+            if (!helpStack.empty()) {
+                helpStack.pop();
+            } else {
+                return -1;
+            }
+            return srcStack.pop();
+        }
+
+        public int min() {
+            if (!helpStack.empty()) {
+                helpStack.peek();
+            } else {
+                return -1;
+            }
+            return helpStack.peek();
+        }
     }
 }
