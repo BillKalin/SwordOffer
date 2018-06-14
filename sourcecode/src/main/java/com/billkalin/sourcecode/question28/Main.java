e8cc495edd440669395fabc480fb527fb60e3c5f
BillKalin <396116131@qq.com>
2018-06-14 20:19:01 +0800

    init question 28

--- sourcecode/src/main/java/com/billkalin/sourcecode/question28/Main.java
+++ sourcecode/src/main/java/com/billkalin/sourcecode/question28/Main.java
@@ -1,9 +1,18 @@
 package com.billkalin.sourcecode.question28;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        int[] array = {4,5,1,6,2,7,3,8};
+        int k = 4;
+        int[] smallArray = findKnum(array, k);
+        System.out.println("数组:" + Arrays.toString(array) + " 最小的" + k + "个数是：" + Arrays.toString(smallArray));
     }
 
+    private static int[] findKnum(int[] array, int k) {
+
+        return new int[]{};
+    }
 }
